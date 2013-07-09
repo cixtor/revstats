d4d77a3549c1dd049b97af44e477d0bfbdf587f0
Cixtor <cixtords@gmail.com>
2013-07-09 09:17:48 -0500

    Added project description and copyright in the script header

--- revstats.js
+++ revstats.js
@@ -1,4 +1,31 @@
 #!/usr/bin/env node
+
+/**
+ * Revision Control Statistics
+ *
+ * http://cixtor.com/
+ * https://github.com/cixtor/revstats
+ * https://en.wikipedia.org/wiki/Revision_control
+ * https://en.wikipedia.org/wiki/Git_(software)
+ * https://en.wikipedia.org/wiki/Mercurial
+ * https://en.wikipedia.org/wiki/Apache_Subversion
+ *
+ * A component of software configuration management, revision control, also
+ * known as version control or source control, is the management of changes to
+ * documents, computer programs, large web sites, and other collections of
+ * information. Changes are usually identified by a number or letter code, each
+ * revision is associated with a timestamp and the person making the change.
+ *
+ * The profile contributions graph is a record of contributions you have made to
+ * VCS (Version Control System) repositories but they are only counted if they
+ * meet certain criteria. Commits will appear on your contributions graph if
+ * they meet all of the following conditions:
+ *
+ * - The commits were made within the past year.
+ * - The commits were made in a standalone repository, not a fork.
+ * - The commits were made in the repository's default branch.
+ */
+
 var fsys = require('fs');
 var exec = require('child_process').spawnSync;
 

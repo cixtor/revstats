3f78b52acd83e74bc6facfbfcdccc6b4a139e454
Cixtor <cixtords@gmail.com>
2013-07-29 10:56:10 -0500

    Added date helpers for the commit history processor

--- revstats.js
+++ revstats.js
@@ -3,6 +3,24 @@ var exec = require('child_process').spawnSync;
 
 var projects = [process.argv[2]];
 
+var secondsPerDay = function() {
+    return 86400 /* 60 secs * 60 mins * 24 hours */;
+};
+
+var yyyymmdd = function(ts, gmt) {
+    var dref = new Date(ts * 1000);
+
+    if (gmt === true) {
+        return dref.toString();
+    }
+
+    var year = dref.getFullYear();
+    var month = ('0' + (dref.getMonth() + 1)).slice(-2);
+    var day = ('0' + dref.getDate()).slice(-2);
+
+    return (year + '-' + month + '-' + day);
+};
+
 var getAllCommits = function (projects, callback) {
     var folder;
     var gitstats;
